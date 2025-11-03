bdfc77ad136261675fbd6d8e4208c57711308e94
leehojun <junvery2@naver.com>
2025-11-03 17:39:52 +0900

    d

--- pages/3) 얼굴 확인/카메라로_확인.py
+++ pages/3) 얼굴 확인/카메라로_확인.py
@@ -29,6 +29,7 @@ class VideoProcessor(VideoProcessorBase):
                 label = result.get("id", "unknown")  # ✅ 대표 모델만 선택
             else:
                 label = "Many People"
+                print(result)
         except Exception as e:
             print("🔥 예외 발생:", e)  # ✅ 콘솔에 에러 메시지 출력
             label = "Error except"
