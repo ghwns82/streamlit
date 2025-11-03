5409b314a26d953a680efff8329ca38fed7cf59b
leehojun <junvery2@naver.com>
2025-11-03 22:25:29 +0900

    d

--- pages/4) 출석체크/디버그용_출석_확인.py
+++ pages/4) 출석체크/디버그용_출석_확인.py
@@ -4,23 +4,22 @@ import os
 from config import BACK_URL
 
 st.set_page_config(page_title="출석체크", page_icon="📤")
-st.title("🕒 출석체크확인")
+st.title("🕒 디버그용 출석체크확인")
 
 API_URL =BACK_URL+'/attendance'
 
 
 with st.form("upload_form"):
-    name = st.text_input("이름 (필수,영문)")
-    ID = st.text_input("학번 (필수)")
+    student_id = st.text_input("학번 (필수)")
     submitted = st.form_submit_button("전송")
 
 if submitted:
-    if not ID:
+    if not student_id:
         st.error("학번은 필수입니다.")
     else:
         try:          
             # 폼 데이터: 키 이름은 반드시 'text'
-            data = {"ID": ID}
+            data = {"student_id": student_id}
 
             with st.spinner("전송 중..."):
                 resp = requests.post(API_URL, data=data, timeout=60)
